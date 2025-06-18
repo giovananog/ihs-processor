6b6ec7c180bfd66996f890a9dde8ae8892889ab9
giovananog <giovana.nogueira@sou.unifal-mg.edu.br>
2025-06-17 23:29:29 -0300

    update existing tests and add test4

--- test/test2.v
+++ test/test2.v
@@ -1,13 +1,40 @@
-initial begin
-  $dumpfile("testbench.vcd");
-  $dumpvars(0, testbench);
+// test2.v
 
-  resetn = 1'b0;
-  #2 resetn = 1'b1;
+`include "../src/counter.v"
+`include "../src/register.v"
+`include "../src/signal_extender.v"
+`include "../src/alu.v"
+`include "../src/decoder.v"
+`include "../src/control_unit.v"
+`include "../src/processor.v" 
 
-  #8 iin = 16'b1010110000000100;  // ldi r3, #4
-  #8 iin = 16'b1011000000001010;  // ldi r4, #10
-  #8 iin = 16'b0011010000110000;  // sub r5, r4
-  #8 iin = 16'b1001010000000000;  // out r5
-  #8 $finish;
-end
+module testbench;
+    reg clock = 1'b0;      
+    reg [15:0] iin;        
+    reg resetn = 1'b1;     
+    wire [15:0] bus;       
+
+    processor p(
+        .clock(clock),
+        .iin(iin),
+        .resetn(resetn),
+        .bus(bus)
+    );
+
+    always #1 clock = !clock;
+
+    initial begin
+      $dumpfile("testbench.vcd");
+      $dumpvars(0, testbench);
+
+      resetn = 1'b0;
+      #2 resetn = 1'b1;
+
+      #8 iin = 16'b1010000000101010;  // ldi r0, #42
+      #8 iin = 16'b1110010000000000;  // rep r1, r0
+      #8 iin = 16'b1000010000000000;  // out r1
+      #8 $finish;
+    end
+
+
+endmodule
--- test/test3.v
+++ test/test3.v
@@ -1,13 +1,41 @@
-initial begin
-  $dumpfile("testbench.vcd");
-  $dumpvars(0, testbench);
+// test3.v
 
-  resetn = 1'b0;
-  #2 resetn = 1'b1;
+`include "../src/counter.v"
+`include "../src/register.v"
+`include "../src/signal_extender.v"
+`include "../src/alu.v"
+`include "../src/decoder.v"
+`include "../src/control_unit.v"
+`include "../src/processor.v" 
 
-  #8 iin = 16'b1011100000001111;  // ldi r6, #15
-  #8 iin = 16'b1011110011110000;  // ldi r7, #240
-  #8 iin = 16'b0100001110000000;  // nan r0, r7
-  #8 iin = 16'b1000000000000000;  // out r0
-  #8 $finish;
-end
+module testbench;
+    reg clock = 1'b0;      
+    reg [15:0] iin;        
+    reg resetn = 1'b1;     
+    wire [15:0] bus;       
+
+    processor p(
+        .clock(clock),
+        .iin(iin),
+        .resetn(resetn),
+        .bus(bus)
+    );
+
+    always #1 clock = !clock;
+
+    initial begin
+      $dumpfile("testbench.vcd");
+      $dumpvars(0, testbench);
+
+      resetn = 1'b0;
+      #2 resetn = 1'b1;
+
+      #8 iin = 16'b1011100000001111;  // ldi r6, #15   (000Fh)
+      #8 iin = 16'b1011110011110000;  // ldi r7, #240  (00F0h)
+      #8 iin = 16'b0100001101110000;  // nan r0, r6, r7  
+      #8 iin = 16'b1000000000000000;  // out r0        
+      #8 $finish;
+    end
+
+
+endmodule
